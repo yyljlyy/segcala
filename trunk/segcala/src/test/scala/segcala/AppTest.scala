03ac7183695d34be9057307b477efa054cf45023
zhangyf <zhangyf@9ee32df2-f226-11de-945e-730681a4ffd5>
2010-01-06 14:54:36 +0000

    code refactor

--- trunk/segcala/src/test/scala/segcala/AppTest.scala
+++ trunk/segcala/src/test/scala/segcala/AppTest.scala
@@ -2,11 +2,13 @@ package segcala
 
 import org.junit._
 import Assert._
+import com.google.inject.{Injector, Guice}
 
 @Test
 class AppTest {
+
   @Test
-  def testDict() = {
+  def testDict(){
     Dict.addWord("研究")
     Dict.addWord("生命")
     Dict.addWord("科学")
@@ -20,6 +22,22 @@ class AppTest {
     assertTrue(true)
   }
 
+  @Test
+  def testSeg(){
+    val inj:Injector = Guice.createInjector(new SegModule())
+    inj.getInstance(classOf[Dict])
+
+    /*Dict.addWord("研究")
+    Dict.addWord("生命")
+    Dict.addWord("科学")
+    Dict.addWord("科")
+    Dict.addWord("研究生")
+    Dict.addWord("命", 1234)*/
+
+    val words = MMSeg.seg(new TextFragment("研究生命科学"))
+    words.foreach(w => println(w.value))
+
+  }
 
 }
 
